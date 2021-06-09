0f76052c21c70a3e0b89d3a1d29990308da5cced
Muhammad Naufal Rafif Hibatullah <naufif17@gmail.com>
2021-06-09 08:19:59 +0700

    Tugas Shared Preferences

---
+++ UsingPreferences/app/src/main/java/com/example/usingpreferences/RegisterActivity.java
@@ -0,0 +1,88 @@
+package com.example.usingpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText mViewUser, mViewPassword, mViewRepassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mViewUser = findViewById(R.id.et_emailSignUp);
+        mViewPassword = findViewById(R.id.et_passwordSignUp);
+        mViewRepassword = findViewById(R.id.et_passwordSignUp2);
+
+        mViewRepassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_NULL){
+                    razia();
+                    return true;
+                }
+                return false;
+            }
+        });
+        findViewById(R.id.button_signupSignup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                razia();
+            }
+        });
+        }
+    private void razia(){
+        mViewUser.setError(null);
+        mViewPassword.setError(null);
+        mViewRepassword.setError(null);
+        View fokus = null;
+        boolean cancel = false;
+
+        String repassword = mViewRepassword.getText().toString();
+        String user = mViewUser.getText().toString();
+        String password = mViewPassword.getText().toString();
+
+        if(TextUtils.isEmpty(user)){
+            mViewUser.setError("Tolong masukkan username");
+            fokus = mViewUser;
+            cancel = true;
+        }else if(cekUser(user)){
+            mViewUser.setError("Username telah terdaftar");
+            fokus = mViewUser;
+            cancel = true;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            mViewPassword.setError("Tolong masukkan password");
+            fokus = mViewPassword;
+            cancel = true;
+        }else if(!cekPassword(password,repassword)){
+            mViewRepassword.setError("Password tidak sesuai");
+            fokus = mViewRepassword;
+            cancel = true;
+        }
+
+        if(cancel){
+            fokus.requestFocus();
+        }else{
+            Preferences.setRegisteredUser(getBaseContext(),user);
+            Preferences.setRegisteredPass(getBaseContext(),password);
+            finish();
+        }
+    }
+    private boolean cekPassword(String password, String repassword){
+        return password.equals(repassword);
+    }
+    private boolean cekUser(String user){
+        return user.equals(Preferences.getRegisteredUser(getBaseContext()));
+    }
+}
